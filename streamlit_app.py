fe01cc9b23b65bc2f0393f4d35b750c95965630e
reeteshagarwal <30353178+reeteshagarwal@users.noreply.github.com>
2023-09-22 16:45:01 -0400

    Update streamlit_app.py
    Added some more goodies !

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale ')
-streamlit.text('Hard-Bolied Eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗 Kale ')
+streamlit.text('🐔  Hard-Bolied Eggs')
+streamlit.text('🥑🍞 Avocado Toast')
