ab1974d1c85f9fa3456206c4d13b71bb36ab0fe7
reeteshagarwal <30353178+reeteshagarwal@users.noreply.github.com>
2023-09-22 16:48:01 -0400

    Update streamlit_app.py
    introduced Pandas data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -6,3 +8,6 @@ streamlit.text ('🥗 Kale ')
 streamlit.text('🐔  Hard-Bolied Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
