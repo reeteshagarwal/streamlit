a27a2fb6264f82c7309897d78db990f8c1a4adb0
reeteshagarwal <30353178+reeteshagarwal@users.noreply.github.com>
2023-09-26 08:19:23 -0400

    Update streamlit_app.py
    Testing Snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 
-
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -31,3 +30,10 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choic
 streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
